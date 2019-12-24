3433aeb3cf247a4f1cca9f6f1bab2e01c2a59dbb
Felipe <felipekid@gmail.com>
2019-12-24 15:49:27 +0100

    Game ready

--- Game Pong/pong.py
+++ Game Pong/pong.py
@@ -1,34 +1,34 @@
-# Part 1: Getting Started
-# Module that work good with simple games, normaly people use pygame
-import turtle
+import turtle # for simple games it works fine, but normaly people uses pygame
+import os # to be able to blay the sound when the ball bounce
+import tkinter as tk
 
-wm = turtle.Screen()
-wm.title("Pong with Python")
-wm.bgcolor("black")
-wm.setup(width=800, height=600)
-# that stops the window to automatic update and serves to speed up the game
-wm.tracer(0)
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0) #that stops the windows to automatic update and serves to speed up the game
 
+# Score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
-
 
 # Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
-
 
 # Ball
 ball = turtle.Turtle()
@@ -40,38 +40,86 @@ ball.goto(0, 0)
 ball.dx = 2
 ball.dy = 2
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player 1: 0  Player 1: 0", align="center", font=("Courier", 24, "normal"))
 
-# Function
+# Functions
 def paddle_a_up():
-	y = paddle_a.ycor()
-	y += 20
-	paddle_a.sety(y)
-	
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
 def paddle_a_down():
-	y = paddle_a.ycor()
-	y -= 20
-	paddle_a.sety(y)
-	
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
 
 def paddle_b_up():
-	y = paddle_b.ycor()
-	y += 20
-	paddle_b.sety(y)
-	
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
 def paddle_b_down():
-	y = paddle_b.ycor()
-	y -= 20
-	paddle_b.sety(y)
-	
-# keyboard binding
-wm.listen()
-wm.onkeypress(paddle_a_up, "w")
-wm.onkeypress(paddle_a_down, "s")
-wm.onkeypress(paddle_b_up, "Up")
-wm.onkeypress(paddle_b_down, "Down")
-	
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 while True:
-	wm.update()
-		
+    wn.update()
+    
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+
+    # Top and bottom
+    if ball.ycor() > 290: # boarder limit is half of the screen (screen size set = 600), minus the ball size from the center of the ball (10px - ball size 20px)
+        ball.sety(290)
+        ball.dy *= -1
+        os.system("afplay bounce.wav&")
+    
+    elif ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        os.system("afplay bounce.wav&")
+
+    # Left and right
+    if ball.xcor() > 350: # boarder limit is half of the screen (screen size set = 800), minus the ball size from the center of the ball (10px - ball size 20px)
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    elif ball.xcor() < -350:
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and ball collisions
+    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.dx *= -1 
+        os.system("afplay bounce.wav&") # the & sign is to prevent the game to freeze, play the sound and unfreeze, now it will play without make the game freeze
+    
+    elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        ball.dx *= -1
+        os.system("afplay bounce.wav&")
+    
